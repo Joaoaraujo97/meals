376740df4f5f91ebed2952b202c8306d6ca8f5d8
Joao Pedro <joaopedro@MacBook-Air-de-Joao.local>
2023-12-13 11:18:37 -0300

    Primeiro commit

---
+++ lib/components/category_item.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:meals/utils/app_routes.dart';
+import '../models/category.dart';
+
+class CategoryItem extends StatelessWidget {
+  const CategoryItem(this.category, {super.key});
+
+  final Category category;
+
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).pushNamed(
+      AppRoutes.CATEGORIES_MEALS,
+      arguments: category,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => _selectCategory(context),
+      borderRadius: BorderRadius.circular(15),
+      splashColor: Theme.of(context).primaryColor,
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            gradient: LinearGradient(colors: [
+              category.color.withOpacity(0.5),
+              category.color,
+            ])),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/favorite_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:meals/components/meal_item.dart';
+import '../models/meal.dart';
+
+class FavoriteScreen extends StatelessWidget {
+  const FavoriteScreen(this.favoriteMeals, {super.key});
+
+  final List<Meal> favoriteMeals;
+
+  @override
+  Widget build(BuildContext context) {
+    if (favoriteMeals.isEmpty) {
+      return const Center(
+        child: Text('Nenhuma refeição foi marcada como favorita!'),
+      );
+    } else {
+      return ListView.builder(
+        itemCount: favoriteMeals.length,
+        itemBuilder: ((context, index) {
+          return MealItem(favoriteMeals[index]);
+        }),
+      );
+    }
+  }
+}
